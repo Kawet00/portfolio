865310b7f547d7626f0378f6a8249a4bb5f6a3f9
Kawet <69346261+Kawet00@users.noreply.github.com>
2025-04-27 11:02:27 +0200

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage.js";
-import { insertProfileSchema, insertProjectSchema } from "@shared/schema";
+import { insertProfileSchema, insertProjectSchema } from "../shared/schema.js";
 import { ZodError } from "zod";
 import { sendContactNotification } from "./discord.js";
 
