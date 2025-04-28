00252bbb412db9db728a011e2e765cd25dce3be9
Kawet <elpisstolero13@outlook.com>
2025-04-28 08:39:53 +0200

    tqt

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,10 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
+  esbuild: {
+    target: 'esnext',
+    platform: 'linux'
+  },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
